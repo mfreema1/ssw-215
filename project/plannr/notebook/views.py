b61e95db06ddbdd18256f96317d2683dc57c2bcf
mfreema1 <mfreema1@stevens.edu>
2018-04-26 13:43:50 -0400

    Add filtering of results based on user

--- project/plannr/notebook/views.py
+++ project/plannr/notebook/views.py
@@ -19,11 +19,18 @@ def index(request):
 # needs to be filled
 
 # add our mixin to enforce logging in for our users
+# for our generic, class based views, we'll need to override
+# our def get_queryset(self) function inside of the class
+# the class based view calls this itself when it needs to grab
+# the model
 from django.views.generic.list import ListView
 class DailyListView(LoginRequiredMixin, ListView):
     model = DailyEntry
     login_url = '/accounts/login/'
+    #order this by date
     paginate_by = 10
+    def get_queryset(self):
+        return DailyEntry.objects.filter(author=self.request.user)
 
 from django.views.generic.detail import DetailView
 class DailyDetailView(LoginRequiredMixin, DetailView):
